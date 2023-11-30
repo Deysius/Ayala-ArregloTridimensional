c09f9f833b9ffc6d9b924066fbf60651d7a58077
Josue Ayala <josue.ayala.flores@udla.edu.ec>
2023-11-30 16:48:16 -0500

    Incluyo mis vectores

---
+++ Arreglo Tridimencional.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int filas, columnas,num;
+    printf("Ingrese el número de filas de la matriz: ");
+    scanf("%d", &filas);
+    printf("Ingrese el número de columnas de la matriz: ");
+    scanf("%d", &columnas);
+    printf("Numero de matrices: ");
+    scanf("%d", &num);
+    printf("Primera Matriz:\n");
+    int matriz[filas][columnas][num];
+}
